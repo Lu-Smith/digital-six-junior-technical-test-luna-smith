c7ba2f3aeeb2b85be7d5cc2956dc6e3554dd1bca
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 14:52:26 +0000

    Updated TopBanner.Section.js
    Added Shop now button

--- bright-water/src/components/TopBannerSection/TopBannerSection.js
+++ bright-water/src/components/TopBannerSection/TopBannerSection.js
@@ -6,6 +6,7 @@ export default function TopBannerSection({ topBannerImage }) {
   return (
     <div className="TopBannerSection">
         <h1>REUSE RESET REFRESH</h1>
+        <button>Shop now</button>
     </div>
   );
 }
