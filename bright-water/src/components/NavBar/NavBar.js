7a4d1f5d7f1df578e8a9573ebfb028c1c3b65f21
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 10:33:13 +0000

    Updated NavBar.js

--- bright-water/src/components/NavBar/NavBar.js
+++ bright-water/src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import classes from "./navBar.sass";
 export default function NavBar({ navItems }) {
   return (
     <div>
-      <h2>NavBar Component</h2>
+      <h2>Bright Water</h2>
       <ul>
         {navItems.map((item, index) => (
           <li key={index}>
