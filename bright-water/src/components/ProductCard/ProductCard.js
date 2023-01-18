16ea31a8e5bb383e1727fd81f797b679c2b307b8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 14:22:33 +0000

    Updated ProductCard.js

--- bright-water/src/components/ProductCard/ProductCard.js
+++ bright-water/src/components/ProductCard/ProductCard.js
@@ -1,27 +1,22 @@
-import React, { useEffect, useState} from 'react';
+
 import ViewProductButton from "../ViewProductButton/ViewProductButton";
 import classes from "./productCard.sass";
 
 export default function ProductCard({ product }) {
-  console.log(product);
-  const [description, setDescription] = useState(product.description);
+  console.log(product.price)
 
-  useEffect(() => {
-    const regex = /<p>/gi;
-    const newString = description.replace(regex, " ");
-    setDescription(newString);
-  }, []);
- 
   //Example of a product card: https://dribbble.com/shots/15208151-Furniture-shop-Product-card-design
   return (
     <>
       <h4>{product.name}</h4>
       <img src={product.image.url} alt={product.name} />
+      <h3>${product.price.formatted} </h3>
       
-      {description}
+      
+    
       
       
-      <ViewProductButton price={product.price} checkoutImage={product.checkout_url}/>
+      <ViewProductButton price={product.price.formatted} checkoutImage={product.checkout_url}/>
     </>
   );
 }
