8e99a994c9039c2d0b574231daf14188d59081a3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 15:21:25 +0000

    Updated SupportBannerSection.js and added to Home.js
    Updated supportBannerSection.sass

--- bright-water/src/Home.js
+++ bright-water/src/Home.js
@@ -1,12 +1,14 @@
 import TopBannerSection from "./components/TopBannerSection/TopBannerSection";
 import ProductsList from "./components/ProductList/ProductList";
 import classes from "./home.sass";
+import SupportBannerSection from "./components/SupportBannerSection/SupportBannerSection";
 
 export default function Home({ productData }) {
   console.log("Product Data", productData);
   return (
     <>
       <TopBannerSection />
+      <SupportBannerSection />
       <ProductsList products={productData}/>
     </>
   );
--- bright-water/src/components/SupportBannerSection/SupportBannerSection.js
+++ bright-water/src/components/SupportBannerSection/SupportBannerSection.js
@@ -3,9 +3,9 @@ import classes from "./supportBannerSection.sass";
 
 export default function SupportBannerSection() {
 
-  return (
-    <div className="SupportBannerSection">
-        <h2>A Reusable Way of Living</h2>
-    </div>
+  return( 
+        <div className="SupportBannerSection">
+            <h2>A Reusable Way of Living</h2>
+        </div>
   );
 }
