fe14083b83747ab2885f7133500ce38a63a8f91a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 17:25:45 +0000

    Updated ProductCard.js

--- bright-water/src/components/ProductCard/ProductCard.js
+++ bright-water/src/components/ProductCard/ProductCard.js
@@ -7,16 +7,11 @@ export default function ProductCard({ product }) {
 
   //Example of a product card: https://dribbble.com/shots/15208151-Furniture-shop-Product-card-design
   return (
-    <>
+    <div className="ProductCard">
       <h4>{product.name}</h4>
       <img src={product.image.url} alt={product.name} />
-      <h3>${product.price.formatted} </h3>
-      
-      
-    
-      
-      
+      <h3>${product.price.formatted}</h3>
       <ViewProductButton price={product.price.formatted} checkoutImage={product.checkout_url}/>
-    </>
+    </div>
   );
 }
