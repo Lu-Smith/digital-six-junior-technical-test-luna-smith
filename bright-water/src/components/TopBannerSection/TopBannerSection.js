27e09270163e1cf5e9dd21e92d977dd897c103c5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 14:35:52 +0000

    Updated TopBannerSection.js
    Uploaded an image for the banner

--- bright-water/src/components/TopBannerSection/TopBannerSection.js
+++ bright-water/src/components/TopBannerSection/TopBannerSection.js
@@ -1,6 +1,12 @@
 import classes from "./topBannerSection.sass";
+import Banner from '../../lib/BrightWaterBanner1.jpg';
 
 export default function TopBannerSection({ topBannerImage }) {
   console.log(topBannerImage)
-  return <h1>Top Banner Section</h1>;
+  return (
+    <div className="TopBannerSection">
+        <img src={Banner} alt="banner for Broght Water" />
+        <h1>REUSE RESET REFRESH</h1>
+    </div>
+  );
 }
