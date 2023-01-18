d28490139e0d3012bb88e107334b778340776cc5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 15:11:03 +0000

    Created SupportBannerSection.js and sass

---
+++ bright-water/src/components/SupportBannerSection/SupportBannerSection.js
@@ -0,0 +1,11 @@
+import classes from "./supportBannerSection.sass";
+
+
+export default function SupportBannerSection() {
+
+  return (
+    <div className="SupportBannerSection">
+        <h2>A Reusable Way of Living</h2>
+    </div>
+  );
+}
