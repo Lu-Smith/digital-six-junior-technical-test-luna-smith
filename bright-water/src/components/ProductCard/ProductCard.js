98ecb586a5b6e8a278410c56a096f1b6849a5e72
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 18:20:05 +0000

    Updated ProductCard.js

--- bright-water/src/components/ProductCard/ProductCard.js
+++ bright-water/src/components/ProductCard/ProductCard.js
@@ -3,7 +3,7 @@ import ViewProductButton from "../ViewProductButton/ViewProductButton";
 import classes from "./productCard.sass";
 
 export default function ProductCard({ product }) {
-  console.log(product.price)
+  console.log(product.permalink)
 
   //Example of a product card: https://dribbble.com/shots/15208151-Furniture-shop-Product-card-design
   return (
@@ -12,7 +12,7 @@ export default function ProductCard({ product }) {
       <div>
           <h4>{product.name}</h4>
           <h3>${product.price.formatted}</h3>
-          <ViewProductButton price={product.price.formatted} checkoutImage={product.checkout_url}/>
+          <ViewProductButton productPermaLink={product.permalink}/>
       </div>
     </div>
   );
--- bright-water/src/components/TopBannerSection/TopBannerSection.js
+++ bright-water/src/components/TopBannerSection/TopBannerSection.js
@@ -2,7 +2,6 @@ import classes from "./topBannerSection.sass";
 
 
 export default function TopBannerSection({ topBannerImage }) {
-  console.log(topBannerImage)
   return (
     <div className="TopBannerSection">
         <h1>REUSE RESET REFRESH</h1>
