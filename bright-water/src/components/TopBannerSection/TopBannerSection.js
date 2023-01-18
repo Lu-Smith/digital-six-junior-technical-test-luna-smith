b19bade86a6a128995a5055fce15efe120cfa49e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 14:45:21 +0000

    Updated topBannerSection.sass
    Removed the image element from TopBannerSecction.js

--- bright-water/src/components/TopBannerSection/TopBannerSection.js
+++ bright-water/src/components/TopBannerSection/TopBannerSection.js
@@ -1,11 +1,10 @@
 import classes from "./topBannerSection.sass";
-import Banner from '../../lib/BrightWaterBanner1.jpg';
+
 
 export default function TopBannerSection({ topBannerImage }) {
   console.log(topBannerImage)
   return (
     <div className="TopBannerSection">
-        <img src={Banner} alt="banner for Broght Water" />
         <h1>REUSE RESET REFRESH</h1>
     </div>
   );
