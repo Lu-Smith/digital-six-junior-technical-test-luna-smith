4b320c75ab9b6c52f5741fb746f0e2bdef6f7a95
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-18 15:24:22 +0000

    Updated ProductList.js

--- bright-water/src/components/ProductList/ProductList.js
+++ bright-water/src/components/ProductList/ProductList.js
@@ -4,12 +4,12 @@ import classes from "./productList.sass";
 //Example of a product list: https://www.edinburghgin.com/shop/london-dry
 export default function ProductList({ products }) {
   return (
-    <>
+    <div className="ProductList">
       <h2>Trending now</h2>
       {products.map((product, index) => {
         return <ProductCard key={index} product={product}/>
       })}
       
-    </>
+    </div>
   );
 }
