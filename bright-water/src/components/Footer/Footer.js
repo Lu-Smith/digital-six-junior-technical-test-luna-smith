c861a14214a4886070ac6efb6216c728d7d62334
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-19 18:57:51 +0000

    Updated Footer.js

--- bright-water/src/components/Footer/Footer.js
+++ bright-water/src/components/Footer/Footer.js
@@ -1,17 +1,47 @@
 import { Link } from "react-router-dom";
+import { footerContent } from "../../lib/footerContent";
 import classes from "./footer.sass";
 
 export default function Footer({ navItems }) {
   return (
     <div className="Footer">
-      <h2>Bright Water</h2>
-      <ul>
-        {navItems.map((item, index) => (
-          <li key={index}>
-            <Link to={item.link}>{item.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div>
+          <h2>Bright Water</h2>
+          <ul>
+            {navItems.map((item, index) => {
+                  if (item.name === "Basket") {
+                      return <li key={index}>
+                                <Link to={item.link}>
+                                    <span className="material-symbols-outlined">
+                                        shopping_basket
+                                    </span>
+                                </Link>
+                            </li>
+                  } else {
+                      return <li key={index}>
+                                <Link to={item.link}>{item.name}</Link>
+                            </li>
+                  }
+                  }
+            )}
+          </ul>
+      </div>
+      <div>
+        {footerContent.map((content, index) => {
+          return (
+            <ul key={index}>
+              <ul>
+                <li className="title">{content.title}</li>
+                <li>{content.filler1}</li>
+                <li>{content.filler2}</li>
+                <li>{content.filler3}</li>
+              </ul>
+            </ul>
+          )
+        })}
+      </div>
+        
+       
     </div>
   );
 }
---
+++ bright-water/src/lib/footerContent.js
@@ -0,0 +1,20 @@
+export const footerContent = [
+    {
+        title: "Help",
+        filler1: "Frequently Asl=ked Questions",
+        filler2: "Reviews",
+        filler3: "Contact Us"
+    },
+    {
+        title: "Shop",
+        filler1: "Gift Sets",
+        filler2: "Travel Sets",
+        filler3: "Accessories"
+    },
+    {
+        title: "About",
+        filler1: "Press",
+        filler2: "Our Story",
+        filler3: "Affiliate program"
+    }
+]
