948c4d7a5a5ea8da24831bd237ea9d9f0320e3cb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-19 23:36:58 +0000

    Updated NavBar.js and saas

--- bright-water/src/components/NavBar/NavBar.js
+++ bright-water/src/components/NavBar/NavBar.js
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./navBar.sass";
 
 export default function NavBar({ navItems }) {
+
+  const [showNavBarLinks, setShowNavBarLinks] = useState(false);
+
+  const toggleNavBarLinks = () => {
+    setShowNavBarLinks(!showNavBarLinks);
+  }
   
   return (
     <div className="NavBar">
       <h2>Bright Water</h2>
-      <ul>
+      <button className="mobileMenu" onClick={toggleNavBarLinks}>Toggle Navbar</button>
+      <ul className={`navBarLinks ${showNavBarLinks ? "" : "hidden"}`}>
         {navItems.map((item, index) => {
           if (item.name === "Basket") {
             return <li key={index}>
