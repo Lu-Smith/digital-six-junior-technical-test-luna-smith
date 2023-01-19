6932962f27dfd06dde9cbdb0d3f7997fe6d6651b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-19 11:35:44 +0000

    Updated ProductCard.js

--- bright-water/src/ProductPage.js
+++ bright-water/src/ProductPage.js
@@ -31,7 +31,7 @@ export default function ProductPage() {
 
   useEffect(() => {
     fetchProduct();
-  }, []);
+  }, [product]);
 
 
   return (
--- bright-water/src/components/ProductCard/ProductCard.js
+++ bright-water/src/components/ProductCard/ProductCard.js
@@ -3,7 +3,6 @@ import ViewProductButton from "../ViewProductButton/ViewProductButton";
 import classes from "./productCard.sass";
 
 export default function ProductCard({ product }) {
-  console.log(product.permalink)
 
   //Example of a product card: https://dribbble.com/shots/15208151-Furniture-shop-Product-card-design
   return (
