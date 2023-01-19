7bd559a0e32635d9f27ea7e3c2e4e3ff652f32df
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-19 23:03:22 +0000

    Updated About.js and saas

--- bright-water/src/About.js
+++ bright-water/src/About.js
@@ -2,8 +2,23 @@ import classes from "./about.sass";
 
 export default function About() {
   return (
-    <>
-      <h1>About Page</h1>
-    </>
+    <div className="About">
+      <h1>Welcome to Bright Water!</h1>
+      <p>
+         At Bright Water, we believe in the power of small actions to make a big impact. That's why we offer a wide selection of high-quality reusable water bottles and drink accessories to help you stay hydrated and refreshed, all while reducing your environmental footprint.
+      </p>
+      <p>
+         Our motto, "reuse, reset, and refresh," encapsulates our mission to promote sustainability and encourage responsible consumption. We believe that by making small changes in our daily lives, we can collectively make a big difference for our planet.
+      </p>
+      <p>
+         Our water bottles are made from durable, BPA-free materials and are designed to last for years. They come in a variety of sizes, colors, and styles to suit any taste and need. Our drink accessories include a range of items, such as carabiners, insulated sleeves and straws.
+      </p>
+      <p>
+         We are dedicated to providing our customers with top-notch customer service and fast shipping. We are constantly searching for new, innovative products to add to our collection, so be sure to check back often to see what's new.
+      </p>
+      <p>
+         Thank you for choosing Bright Water. Together, we can make a difference.
+      </p>
+    </div>
   );
 }
