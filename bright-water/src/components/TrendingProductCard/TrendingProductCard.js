8827dab56448f1efa81c940a57788f0457579792
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-20 16:27:56 +0000

    Updated slide gallery in trendingProductCard.js and saas
    Added more google icons, prev and next

--- bright-water/src/components/TrendingProductCard/TrendingProductCard.js
+++ bright-water/src/components/TrendingProductCard/TrendingProductCard.js
@@ -1,18 +1,38 @@
+import React, { useState } from "react";
 import { content } from "../../lib/trendingContent";
 import classes from "./trendingProductCard.sass";
 
-export default function trendingProductCard({ product }) {
+export default function TrendingProductCard() {
+
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const handleNext = () => {
+    setActiveSlide(prevActiveSlide => (prevActiveSlide + 1) % content.length);
+  }
+  const handlePrev = () => {
+    setActiveSlide(prevActiveSlide => (prevActiveSlide - 1 + content.length) % content.length);
+  }
 
   //Example of a product card: https://dribbble.com/shots/15208151-Furniture-shop-Product-card-design
   return (
     <div className="TrendingProductCard">
+      <button onClick={handlePrev} className="prevButton">
+        <span class="material-symbols-outlined">
+      arrow_back_ios
+        </span>
+      </button>
       {content.map((trendImage, index) => {
         return (
-          <div key={index}>
+          <div key={index} style={{ display: (activeSlide === index) ? 'block' : 'none' }}>
               <img src={trendImage.src} alt={trendImage.alt} />
               <h3>{trendImage.alt}</h3>
           </div>)
       })}
+      <button onClick={handleNext} className="nextButton">
+        <span class="material-symbols-outlined">
+          arrow_forward_ios
+        </span>
+      </button>
     </div>
   );
 }
